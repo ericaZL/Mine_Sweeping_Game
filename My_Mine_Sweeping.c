bf82ebb240dee4bc1f2a528f279a5a2ae8c81410
Ziying (Erica) Long <z5long@edu.uwaterloo.ca>
2017-09-09 00:51:56 -0400

    display results based on the progress of the game
    used queue (linked list) to store the positions of mines (for the result displays)

--- My_Mine_Sweeping.c
+++ My_Mine_Sweeping.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <assert.h>
 #include<time.h>
-#include "My_Mine_Sweeping.h"
 
 // Source: srand() and rand() functions in set_mine(mine); from http://www.tutorialspoint.com/c_standard_library/c_function_rand.htm
 
 int row = 9;
 int col = 9;
 int num = 10;
+
+struct posn {
+    int x;
+    int y;
+};
+
+struct llnode {
+    struct posn pos;
+    struct llnode *next;
+};
+
+struct llist {
+    struct llnode *front;
+    struct llnode *back;
+};
 
 void init_board(char minemap[row][col], char cur[row][col]) {
     for (int i = 1; i < row + 1; i++) {
@@ -40,16 +54,63 @@ void display_board(char cur[row][col]) {
 }
 
 
-void set_mine(char minemap[row][col]) {
+// queue:
+struct llist *mlst_create() { // a linked list storing mine positions
+    struct llist *mlst = malloc(sizeof(struct llist));
+    mlst->front = NULL;
+    mlst->back = NULL;
+    return mlst;
+}
+
+
+void mlst_destroy(struct llist *mlst) {
+    struct llnode *curnode = mlst->front;
+    while (curnode) {
+        struct llnode *nextnode = curnode->next;
+        free(curnode);
+        curnode = curnode->next;
+    }
+    free(mlst);
+}
+
+
+void mlst_add_back(struct posn p, struct llist *mlst) {
+    struct llnode *mine = malloc(sizeof(struct llnode));
+    mine->pos = p;
+    mine->next = NULL;
+    if (mlst->front == NULL) {
+        mlst->front = mine;
+    } else {
+        mlst->back->next = mine;
+    }
+    mlst->back = mine;
+}
+
+
+struct llist *set_mine(char minemap[row][col], struct llist *mlst) {
     srand((unsigned)time(NULL));
     for(int count = num; count > 0; ) {
         int x = rand() % row + 1;
         int y = rand() % col + 1;
         if (minemap[x][y] == '-') {
             minemap[x][y] = '*';
+            struct posn p = {x, y};
+            mlst_add_back(p, mlst);
             --count;
         }
     }
+    return mlst;
+}
+
+
+char** display_mine(char cur[row][col], struct llist *mlst) {
+    assert(mlst->front);
+    struct llnode *mine = mlst->front;
+    while (mine) {
+        cur[mine->pos.x][mine->pos.y] = '*';
+        mine = mine->next;
+    }
+    return cur;
 }
 
 
@@ -68,7 +129,7 @@ int count_mine(char minemap[row][col], int x, int y) {
 }
 
 
-void mine_sweeping(char minemap[row][col], char cur[row][col]) {
+void mine_sweeping(char minemap[row][col], char cur[row][col], struct llist *mlst) {
     int x = 0;
     int y = 0;
     int left = row*col;
@@ -76,14 +137,15 @@ void mine_sweeping(char minemap[row][col], char cur[row][col]) {
         printf("\nPlease choose a coordinate: row(1-row) col(1-col)：\n");
         scanf("%d%d", &x, &y);
         while (x > row || y > col || x < 1 || y < 1) {
-          printf("INVALID. Please try again.\n");
-          printf("Please choose a coordinate: row(1-row) col(1-col)：\n");
-          scanf("%d%d", &x, &y);
+            printf("INVALID. Please try again.\n");
+            printf("Please choose a coordinate: row(1-row) col(1-col)：\n");
+            scanf("%d%d", &x, &y);
         }
         printf("\n");
         if (minemap[x][y] == '*') {
             printf("BOMB! Mine-Reveal-Map: \n");
-            display_board(minemap);
+            display_mine(cur, mlst);
+            display_board(cur);
             return;
         } else {
             --left;
@@ -92,6 +154,9 @@ void mine_sweeping(char minemap[row][col], char cur[row][col]) {
             display_board(cur);
         }
     }
+    printf("\n");
+    display_mine(cur, mlst);
+    display_board(cur);
     printf("SUCCESS! Congrats :)!\n");
 }
 
@@ -127,13 +192,15 @@ void play_mine_sweeping() {
             printf("# of mines (1-row*col):");
             scanf("%d", &num);
         }
-    } 
+    }
+    struct llist *mines = mlst_create();
     char **minemap = malloc(sizeof(char) * (row+1) * (col+1));
     char **cur = malloc(sizeof(char) * (row+1) * (col+1));
     init_board(minemap, cur);
     display_board(cur);
-    set_mine(minemap);
-    mine_sweeping(minemap, cur);
+    set_mine(minemap, mines);
+    mine_sweeping(minemap, cur, mines);
     free(minemap);
     free(cur);
+    mlst_destroy(mines);
 }
